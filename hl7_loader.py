bf5d99b1bd79470b03dc2c613e666ea47f2099cf
Aoldre <aaronoldre@gmail.com>
2024-12-10 13:08:52 -0800

    Update hl7_loader.py
    file_uploader is not supported. switching to upload string and convert to dataframe

--- hl7_loader.py
+++ hl7_loader.py
@@ -13,10 +13,43 @@ st.write(
     """
 )
 
-uploaded_file = st.file_uploader("Choose a file", type = ["csv"])
-if uploaded_file is not None:
-    # convert file to pandas dataframe
-    df = pd.read_csv(uploaded_file)
+def string_to_rows(df, column_name):
+    """Converts items in a string column to separate rows in a DataFrame."""
+    
+    # Split the string into a list of items
+    df[column_name] = df[column_name].str.split(',')
 
-    st.write("Uploaded Data: ")
-    st.write(df)
+    # Explode the list into separate rows
+    df = df.explode(column_name)
+
+    # Reset the index
+    df.reset_index(drop=True, inplace=True)
+
+    return df
+
+
+# Unsupported component error: We removed the component, st.file_uploader, 
+# that appeared here in keeping with our security policy.
+
+# uploaded_file = st.file_uploader("Choose a file", type = ["csv"])
+# if uploaded_file is not None:
+#     # convert file to pandas dataframe
+#     df = pd.read_csv(uploaded_file)
+
+#     st.write("Uploaded Data: ")
+#     st.write(df)
+
+orders = st.text_area("Enter Orders Here: ")
+
+st.write(orders)
+
+
+
+
+# Create a DataFrame
+df = pd.DataFrame({'ORDER_NUMBER': [orders]})
+
+# Convert items in the 'col1' column to separate rows
+df = string_to_rows(df, 'ORDER_NUMBER')
+
+st.write(df)
